2d086b46fc6fcd95c9987a55a6d1c9677232cd4b
Jon Ngai <jon.ngai@gmail.com>
2020-12-11 18:08:08 -0700

    Moving a number of things around to make an easier to use library. WIP. Pt2.

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -11,28 +11,17 @@
 #include <stddef.h>
 #include <string.h>
 
-#include <nlohmann/json.hpp>
-
-#include "spi_messaging.h"
-#include "spi_protocol.h"
-#include "driver/gpio.h"
-
 #include "decode_raw_mobilenet.h"
 #include "esp32_spi_impl.h"
-
-
 #include "spi_api.hpp"
-#include "SpiPacketParser.hpp"
-#include "depthai-shared/datatype/RawImgDetections.hpp"
 
-#include <memory>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
+#define DECODE_RAW_MOBILENET 0
+#define DEBUG_METADATA 0
 
 #define MAX_DETECTIONS 16
-
-#define GPIO_OUTPUT_IO_0    GPIO_NUM_33
-
-using json = nlohmann::json;
 
 static const char* METASTREAM = "spimetaout";
 static const char* PREVIEWSTREAM = "spipreview";
@@ -54,12 +43,12 @@ void alloc_fail(uint32_t size){
 // ----------------------------------------
 // example of decoding raw mobilenet output
 // ----------------------------------------
-void exampleDecodeRawMobilenet(SpiGetMessageResp get_message_resp){
+void exampleDecodeRawMobilenet(SpiGetMessageResp *get_message_resp){
     json j;
 
     Detection dets[MAX_DETECTIONS];
 
-    int num_found = decode_raw_mobilenet(dets, (half *)get_message_resp.data, 0.5f, MAX_DETECTIONS);
+    int num_found = decode_raw_mobilenet(dets, (half *)get_message_resp->data, 0.5f, MAX_DETECTIONS);
     printf("num_found %d \n", num_found);
 
     if(num_found > 0){
@@ -76,76 +65,23 @@ void exampleDecodeRawMobilenet(SpiGetMessageResp get_message_resp){
 // ----------------------------------------
 
 
-
 // ----------------------------------------
-// example of resending a large message as it's received
+// example callback for chunking up large messages.
 // ----------------------------------------
-/*
-uint8_t resend_large_message(const char* stream_name, SpiProtocolInstance* spi_proto_instance, SpiProtocolPacket* spi_send_packet){
-    uint8_t req_success = 0;
+uint32_t example_chunk_recv_size = 0;
+void example_chunk_message(char* received_packet, uint32_t packet_size, uint32_t message_size){
+    example_chunk_recv_size += packet_size;
 
-    // do a get_size before trying to retreive message.
-    SpiGetSizeResp get_size_resp;
-    req_success = spi_get_size(&get_size_resp, GET_SIZE, stream_name);
-    debug_cmd_print("response: %d\n", get_size_resp.size);
+    printf("example_chunk_message called back packet %d/%d\n", example_chunk_recv_size, message_size);
 
-    if(req_success){
-        // send a get message command (assuming we got size)
-        spi_generate_command(spi_send_packet, GET_MESSAGE, strlen(stream_name)+1, stream_name);
-        generic_send_spi((char *)spi_send_packet);
-
-        uint32_t size = get_size_resp.size;
-        uint32_t total_recv = 0;
-        while(total_recv < size){
-            char recvbuf[BUFF_MAX_SIZE] = {0};
-            req_success = generic_recv_spi(recvbuf);
-            if(req_success){
-                if(recvbuf[0]==START_BYTE_MAGIC){
-                    SpiProtocolPacket* spiRecvPacket = spi_protocol_parse(spi_proto_instance, (uint8_t*)recvbuf, sizeof(recvbuf));
-                    uint32_t remaining_data = size-total_recv;
-                    if ( remaining_data < PAYLOAD_MAX_SIZE ){
-                        //--------------------------------------------
-                        //  Transmit data here
-                        //--------------------------------------------
-                        usleep(2000);
-                        debug_cmd_print("mock transmit data... %d/%d\n", total_recv, size);
-                        if(DEBUG_MESSAGE_CONTENTS){
-                            debug_print_hex((uint8_t*)spiRecvPacket->data, PAYLOAD_MAX_SIZE);
-                        }
-                        
-                        total_recv += remaining_data;
-                    } else {
-                        //--------------------------------------------
-                        //  Transmit data here
-                        //--------------------------------------------
-                        usleep(2000);
-                        debug_cmd_print("mock transmit data... %d/%d\n", total_recv, size);
-                        if(DEBUG_MESSAGE_CONTENTS){
-                            debug_print_hex((uint8_t*)spiRecvPacket->data, PAYLOAD_MAX_SIZE);
-                        }
-                        total_recv += PAYLOAD_MAX_SIZE;
-                    }
-
-                }else if(recvbuf[0] != 0x00){
-                    printf("*************************************** got a half/non aa packet ************************************************\n");
-                    req_success = 0;
-                    break;
-                }
-            } else {
-                printf("failed to recv packet\n");
-                req_success = 0;
-                break;
-            }
-        }
+    if(example_chunk_recv_size >= message_size){
+        example_chunk_recv_size = 0;
     }
-
-    return req_success;
 }
-*/
 // ----------------------------------------
 
 
-void runDemo(){
+void run_demo(){
     uint8_t req_success = 0;
 
     SpiApi mySpiApi;
@@ -166,78 +102,40 @@ void runDemo(){
 
     while(1) {
         // ----------------------------------------
-        // example of receiving messages.
+        // basic example of receiving data and metadata from messages.
         // ----------------------------------------
-        // the req_data method allocates memory for the received packet. we need to be sure to free it when we're done with it.
-        SpiGetMessageResp get_message_resp;
-
-        req_success = mySpiApi.req_data(&get_message_resp, METASTREAM);
-        std::vector<uint8_t> data;
-        if(req_success){
-            if(DECODE_MOBILENET){
-                // ----------------------------------------
-                // example of decoding mobilenet (ENABLE DECODE_MOBILENET flag).
-                // ----------------------------------------
-                exampleDecodeRawMobilenet(get_message_resp);
-            } else {
-                // ----------------------------------------
-                // receive raw data 
-                // ----------------------------------------
-                data = std::vector<std::uint8_t>(get_message_resp.data, get_message_resp.data + get_message_resp.data_size);    
+        FullMessage received_msg;
+        uint8_t recv_success = mySpiApi.req_full_msg(&received_msg, METASTREAM);
+        if(recv_success){
+            // example of decoding mobilenet (only for use with raw mobilenet tensor output).
+            if(DECODE_RAW_MOBILENET){
+                exampleDecodeRawMobilenet(&received_msg.raw_data_resp);
             }
-            free(get_message_resp.data);
-        }
 
-        // ----------------------------------------
-        // example of getting message metadata
-        // ----------------------------------------
-        // the req_metadata method allocates memory for the received packet. we need to be sure to free it when we're done with it.
-        SpiGetMessageResp get_meta_resp;
-
-        req_success = mySpiApi.req_metadata(&get_meta_resp, METASTREAM);
-        if(req_success){
+            // print out plain text metadata (for debug purposes).
             if(DEBUG_METADATA){
-                json j = json::from_msgpack(get_meta_resp.data, get_meta_resp.data + get_meta_resp.data_size);
+                json j = json::from_msgpack(received_msg.raw_meta_resp.data, received_msg.raw_meta_resp.data + received_msg.raw_meta_resp.data_size);
                 printf("Unpacked metadata: %s\n", j.dump().c_str());
             }
 
-            // ----------------------------------------
-            // example of parsing out basic ImgDetection type.
-            // ----------------------------------------
-            switch ((dai::DatatypeEnum) get_meta_resp.data_type)
-            {
-            case dai::DatatypeEnum::ImgDetections :
-            {
-                dai::RawImgDetections det;
-                dai::parseMessage(get_meta_resp.data, get_meta_resp.data_size, det);
-
-                for(const auto& det : det.detections){
-                    printf("label: %d, xmin: %f, ymin: %f, xmax: %f, ymax: %f\n", det.label, det.xmin, det.ymin, det.xmax, det.ymax);
-                }
-            }
-            break;
-            
-            default:
-                break;
-            }
-                    
-            free(get_meta_resp.data);
+            // free up resources once you're done with the message.
+            mySpiApi.free_full_msg(&received_msg);
         }
 
 
         // ----------------------------------------
-        // example of retransmitting a jpeg
+        // example of getting large messages a chunk/packet at a time.
         // ----------------------------------------
-        // get jpeg size
-        //req_success = resend_large_message(PREVIEWSTREAM);
+        //mySpiApi.set_chunk_packet_cb(&example_chunk_message);
+        //mySpiApi.chunk_message(METASTREAM);
 
         // ----------------------------------------
         // pop current message/metadata. this tells the depthai to update the info being passed back using the spi_cmds.
         // ----------------------------------------
         req_success = mySpiApi.spi_pop_messages();
         // or you can pop them individually, like this:
-        //req_success = spi_pop_message(&p_status_resp, METASTREAM);
-        //req_success = spi_pop_message(&p_status_resp, PREVIEWSTREAM);
+        //req_success = mySpiApi.spi_pop_message(METASTREAM);
+        //req_success = mySpiApi.spi_pop_message(PREVIEWSTREAM);
     }
 }
 
@@ -248,7 +146,7 @@ void app_main()
     // init spi for the esp32
     init_esp32_spi();
 
-    runDemo();
+    run_demo();
 
     //Never reached.
     deinit_esp32_spi();
